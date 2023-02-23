699d455394bf1b30f2d6ce6e92e74ec4a71d1808
Nandan S <thenandansrinivas@outlook.com>
2023-02-23 16:09:02 +0530

    app route setup

--- server/routes/router.js
+++ server/routes/router.js
@@ -3,10 +3,17 @@ const appService = require('../services/service')
 const appControl = require('../controllers/controller')
 
 route.get('/',appService.home)
+
 route.get('/signin',appService.signin)
-route.get('/signup',appService.signup)
 route.post('/signin',appControl.postSignin)
+
+route.get('/signup',appService.signup)
 route.post('/signup',appControl.postSignup)
+
+route.get('/profile',appService.profile)
+route.get('/logs',appService.logs)
+
+route.get('/panel',appService.panel)
 
 route.get('*',appService.error)
 
